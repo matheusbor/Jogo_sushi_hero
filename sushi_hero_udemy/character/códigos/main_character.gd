d85056f7f4b667513b7ffe1d8da4edc837a712b5
Matheus Borges <matheuslb2003@gmail.com>
2023-10-25 18:58:11 -0300

    adicionando movimentação

--- sushi_hero_udemy/character/códigos/main_character.gd
+++ sushi_hero_udemy/character/códigos/main_character.gd
@@ -1,31 +1,46 @@
 extends CharacterBody3D
+class_name character
 
+const _normal_speed: float = 5.0
+const _sprint_speed: float = 9.0
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var _current_speed: float 
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export_category("Objects")
+@export var _body: Node3D = null
+@export var _spring_arm_offset: Node3D = null
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _physics_process(_delta: float) -> void:
+	_move()
 	move_and_slide()
+
+	
+func _move() -> void:
+	var _input_direction: Vector2 = Input.get_vector(
+		"move_left", "move_right",
+		"move_foward", "move_backward" 
+		)
+	var _direction: Vector3 = transform.basis * Vector3(
+		_input_direction.x, 
+		0,
+		_input_direction.y
+		).normalized()
+	_direction = _direction.rotated(Vector3.UP, _spring_arm_offset.rotation.y)#é o objeto deste código
+	
+	is_running()#definir ela dentro da _move() para ela só executar enquanto o personagem se mover
+	velocity.x = _direction.x * _current_speed
+	velocity.z = _direction.z * _current_speed
+	
+	
+	
+func is_running() -> bool:
+	if Input.is_action_pressed("shift"):
+		_current_speed = _sprint_speed
+		return true 
+	
+	_current_speed = _normal_speed
+	return false
+	
