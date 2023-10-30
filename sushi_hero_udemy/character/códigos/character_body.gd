23bbc92991b4085bd8cc9fee0535b78bd325b94a
Matheus Borges <matheuslb2003@gmail.com>
2023-10-30 13:04:41 -0300

    construção das cenas do jogo

--- sushi_hero_udemy/character/códigos/character_body.gd
+++ sushi_hero_udemy/character/códigos/character_body.gd
@@ -1,9 +1,21 @@
 extends Node3D
 class_name body
 
+@export_category("Objects")
+@export var _character: CharacterBody3D = null
+@export var _animation: AnimationPlayer = null
+
 
 const _lerp_velocity:float = 0.15
 
 func apply_rotation(_velocity: Vector3) -> void:
-	rotation.y = lerp(rotation.y, atan2(-_velocity.x, -_velocity.z), _lerp_velocity)
+	rotation.y = lerp_angle(rotation.y, atan2(-_velocity.x, -_velocity.z), _lerp_velocity)
 
+func animate(_velocity: Vector3) -> void:
+	if _velocity:
+		if _character.is_running():
+			_animation.play("Run") 
+			return
+		_animation.play("Walk")
+		return
+	_animation.play("Idle")
--- sushi_hero_udemy/character/códigos/main_character.gd
+++ sushi_hero_udemy/character/códigos/main_character.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 class_name character
 
+const _myf = preload("res://../character/códigos/character_body.gd")
+
 const _normal_speed: float = 5.0
 const _sprint_speed: float = 9.0
 
@@ -13,11 +15,12 @@ var _current_speed: float
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
+	
 func _physics_process(_delta: float) -> void:
 	_move()
 	move_and_slide()
+	#_body.animate(velocity)
 
-	
 func _move() -> void:
 	var _input_direction: Vector2 = Input.get_vector(
 		"move_left", "move_right",
@@ -30,10 +33,15 @@ func _move() -> void:
 		).normalized()
 	
 	is_running()#definir ela dentro da _move() para ela só executar enquanto o personagem se mover
-	#_direction = _direction.rotated(Vector3.UP, _spring_arm_offset.rotation.y)#é o objeto deste código
+	_direction = _direction.rotated(Vector3.UP, _spring_arm_offset.rotation.y)#é o objeto deste código
 	
-	velocity.x = _direction.x * _current_speed
-	velocity.z = _direction.z * _current_speed
+	if _direction:
+		velocity.x = _direction.x * _current_speed
+		velocity.z = _direction.z * _current_speed
+#		_body.apply_rotation(velocity)
+		return
+	velocity.x = move_toward(velocity.x, 0 , _current_speed)
+	velocity.z = move_toward(velocity.z, 0 , _current_speed)
 	
 	
 func is_running() -> bool:
